bd0bf49535e580af4f4413e852aabac1505ff98c
twtzptr <twtzzz@yandex.ru>
2022-07-05 23:49:29 +0300

    add migration

---
+++ server/src/migrations/20220705185417-init-db-struct.js
@@ -0,0 +1,131 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('PetTypes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING(40),
+        allowNull: false,
+      },
+    })
+
+    await queryInterface.createTable('Roles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING(40),
+        allowNull: false,
+      },
+    })
+
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      login: {
+        type: Sequelize.STRING(40),
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING(40),
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING(40),
+        allowNull: false,
+      },
+      role_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Roles',
+          key: 'id',
+        },
+      },
+    });
+
+    await queryInterface.createTable('Pets', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      breed: {
+        type: Sequelize.STRING(40),
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING(40),
+        allowNull: false,
+      },
+      info: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+        defaultValue: "",
+      },
+      owner_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id',
+        }
+      },
+      type_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'PetTypes',
+          key: 'id',
+        }
+      }
+    })
+
+    await queryInterface.createTable('Visits', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.DATE(40),
+        allowNull: false,
+      },
+      pet_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Pets',
+          key: 'id',
+        }
+      }
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Visits');
+    await queryInterface.dropTable('Pets');
+    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('PetTypes');
+    await queryInterface.dropTable('Roles');
+  }
+};
