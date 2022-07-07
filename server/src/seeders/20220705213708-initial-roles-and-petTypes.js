37d664593c8e821414ff96300c4dbbf5725de901
twtzptr <twtzzz@yandex.ru>
2022-07-07 14:45:28 +0300

    add admin by default

--- server/src/seeders/20220705213708-initial-roles-and-petTypes.js
+++ server/src/seeders/20220705213708-initial-roles-and-petTypes.js
@@ -2,12 +2,39 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('PetTypes', [{ name: 'Кот' }, { name: 'Собака' }, { name: 'Кролик' }]);
-    await queryInterface.bulkInsert('Roles', [{ name: 'Admin' }, { name: 'Client' }]);
+    await queryInterface.bulkInsert('PetTypes', [
+      { name: 'Кот' },
+      { name: 'Собака' },
+      { name: 'Кролик' },
+    ]);
+    await queryInterface.bulkInsert('Roles', [
+      { name: 'Admin' },
+      { name: 'Client' },
+    ]);
+    const adminRole = await queryInterface.sequelize.query(
+      'SELECT * FROM "Roles" WHERE name = ?',
+      {
+        replacements: ['Admin'],
+        type: queryInterface.sequelize.QueryTypes.SELECT,
+      },
+    );
+
+    const adminRoleId = adminRole[0].id;
+
+    await queryInterface.bulkInsert('Users', [
+      {
+        login: 'admin',
+        email: 'admin@admin.ru',
+        password: 'root',
+        name: 'admin',
+        role_id: adminRoleId,
+      },
+    ]);
   },
 
   async down(queryInterface, Sequelize) {
     await queryInterface.bulkDelete('PetTypes', null);
+    await queryInterface.bulkDelete('Users', null);
     await queryInterface.bulkDelete('Roles', null);
   },
 };
