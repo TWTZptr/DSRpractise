cfaaa5c37ea0e095c56fae4c29a467417bb4e153
twtzptr <twtzzz@yandex.ru>
2022-07-07 07:38:46 +0300

    add validation pipe

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
   await app.listen(process.env.APP_PORT);
 }
 bootstrap();
