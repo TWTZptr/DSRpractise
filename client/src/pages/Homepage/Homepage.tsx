486141b80b211b071e24675cfbd14cd31460d78f
twtzptr <twtzzz@yandex.ru>
2022-07-15 22:34:19 +0300

    done with homepage

--- client/src/pages/Homepage/Homepage.tsx
+++ client/src/pages/Homepage/Homepage.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
 import './Homepage.scss';
 import { SignButton } from '../../components/SignButton/SignButton';
 import { Contacts } from '../../components/Contacts/Contacts';
 import { Logo } from '../../components/Logo/Logo';
+import { MainInfo } from '../../components/MainInfo/MainInfo';
 
 export const Homepage = () => {
   return (
@@ -12,8 +12,26 @@ export const Homepage = () => {
         <Contacts />
         <SignButton />
       </header>
-      <main>Clinic info</main>
-      <footer></footer>
+      <main>
+        <MainInfo />
+      </main>
+      <footer>
+        <p>
+          Появились вопросы? <b>Звоните!</b>
+        </p>
+        <p>
+          <a className="contact" href="tel:+7(473)1234567">
+            <b>+7 (473) 123 45 67</b>
+          </a>
+        </p>
+        <p>Или обращайтесь по адресу:</p>
+        <p>
+          <b>г. Воронеж, ул. Ворошилова, д. 22</b>
+        </p>
+        <div>
+          <i>DSR practice 2022</i>
+        </div>
+      </footer>
     </>
   );
 };
