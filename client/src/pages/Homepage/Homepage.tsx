14b31f17455bed4abf2def65f436f7ed4f90050b
twtzptr <twtzzz@yandex.ru>
2022-07-15 23:45:49 +0300

    footer info moved to segregated component

--- client/src/pages/Homepage/Homepage.tsx
+++ client/src/pages/Homepage/Homepage.tsx
@@ -3,6 +3,7 @@ import { SignButton } from '../../components/SignButton/SignButton';
 import { Contacts } from '../../components/Contacts/Contacts';
 import { Logo } from '../../components/Logo/Logo';
 import { MainInfo } from '../../components/MainInfo/MainInfo';
+import { FooterInfo } from '../../components/FooterInfo/FooterInfo';
 
 export const Homepage = () => {
   return (
@@ -16,21 +17,7 @@ export const Homepage = () => {
         <MainInfo />
       </main>
       <footer>
-        <p>
-          Появились вопросы? <b>Звоните!</b>
-        </p>
-        <p>
-          <a className="contact" href="tel:+7(473)1234567">
-            <b>+7 (473) 123 45 67</b>
-          </a>
-        </p>
-        <p>Или обращайтесь по адресу:</p>
-        <p>
-          <b>г. Воронеж, ул. Ворошилова, д. 22</b>
-        </p>
-        <div>
-          <i>DSR practice 2022</i>
-        </div>
+        <FooterInfo />
       </footer>
     </>
   );
