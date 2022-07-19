ea779a91d4098f1f2ecae466e633b54556f988ed
twtzptr <twtzzz@yandex.ru>
2022-07-19 23:27:41 +0300

    auth service refactor

--- client/src/services/authService.ts
+++ client/src/services/authService.ts
@@ -3,12 +3,16 @@ import { URL_BASE } from './constants';
 import { Response } from '../types/Response';
 import { UserRegistrationData } from '../types/UserRegistrationData';
 
+const authApi = axios.create({
+  baseURL: URL_BASE,
+});
+
 export const tryLogin = async (
   login: string,
   password: string,
 ): Promise<Response> => {
   try {
-    const response = await axios.post(`${URL_BASE}/api/auth/login`, {
+    const response = await authApi.post(`/api/auth/login`, {
       login,
       password,
     });
@@ -28,7 +32,7 @@ export const register = async (
   data: UserRegistrationData,
 ): Promise<Response> => {
   try {
-    await axios.post(`${URL_BASE}/api/users`, data);
+    await authApi.post(`/api/users`, data);
     const loginResponse = await tryLogin(data.login, data.password);
     return {
       status: loginResponse.status,
@@ -48,8 +52,23 @@ export const register = async (
   }
 };
 
+export const tryRefresh = async (): Promise<boolean> => {
+  try {
+    const response = await authApi.post(`/api/auth/refresh`);
+    if (response.data.accessToken) {
+      setAccessToken(response.data.accessToken);
+      console.log('got token by refresh');
+      return true;
+    }
+
+    return false;
+  } catch (err) {
+    return false;
+  }
+};
+
 const setAccessToken = (token: string): void => {
-  axios.defaults.headers.common['Authorization'] = token;
+  authApi.defaults.headers.common['Authorization'] = token;
   localStorage.setItem('access_token', token);
 };
 
@@ -57,6 +76,7 @@ export const initAuth = (): boolean => {
   const token = localStorage.getItem('access_token');
   if (token) {
     setAccessToken(token);
+    console.log(`got token from localstorage ${token}`);
     return true;
   }
   return false;
