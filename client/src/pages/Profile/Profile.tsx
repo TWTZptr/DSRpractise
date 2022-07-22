cec176f19b55f1a0ee66c74123261b2189fa2505
twtzptr <twtzzz@yandex.ru>
2022-07-22 20:58:19 +0300

    profile container rework

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -2,19 +2,43 @@ import { useAuth } from '../../hooks/useAuth';
 import './Profile.scss';
 import { UserProfile } from '../../components/UserProfile/UserProfile';
 import { AdminPanel } from '../../components/AdminPanel/AdminPanel';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const Profile = () => {
   const auth = useAuth();
+  const navigate = useNavigate();
+
+  const onLogout = React.useCallback(() => {
+    const logout = async () => {
+      if (auth) {
+        await auth.logout();
+        navigate('/');
+      }
+    };
+    logout();
+  }, [auth, navigate]);
 
   if (!auth) {
     return <>Неизвестная ошибка</>;
   }
 
+  let profileContent: React.ReactNode;
+
   switch (auth.user.role.name) {
     case 'Client':
-      return <UserProfile user={auth.user} />;
+      profileContent = <UserProfile user={auth.user} />;
+      break;
     case 'Admin':
-      return <AdminPanel admin={auth.user} />;
+      profileContent = <AdminPanel admin={auth.user} />;
+      break;
   }
-  return <div>Неизвестная ошибка</div>;
+  return (
+    <div className="profile-container">
+      <button onClick={onLogout} className="logout-button">
+        Выйти
+      </button>
+      {profileContent}
+    </div>
+  );
 };
