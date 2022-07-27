230f3284bde6c5f633fa36b60bdb0e57ee5e2fc7
twtzptr <twtzzz@yandex.ru>
2022-07-27 03:23:19 +0300

    doctors migration

---
+++ server/src/migrations/20220726225518-add-doctors.js
@@ -0,0 +1,56 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Doctors', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING(80),
+        allowNull: false,
+      },
+      phone: {
+        type: Sequelize.STRING(20),
+        allowNull: false,
+      },
+      education: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+        defaultValue: '',
+      },
+      experience: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: '',
+      },
+      achievements: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: '',
+      },
+      service_types: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: '',
+      },
+    });
+
+    await queryInterface.addColumn('Visits', 'doctor_id', {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'Doctors',
+        key: 'id',
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn('Visits', 'doctor_id');
+    await queryInterface.dropTable('Doctors');
+  },
+};
