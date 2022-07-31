fdf6fcd03b10269abf40d1d3fa446b031acb510c
twtzptr <twtzzz@yandex.ru>
2022-07-31 17:37:26 +0300

    add filters on user panel

--- client/src/components/AdminPanel/AdminPanel.tsx
+++ client/src/components/AdminPanel/AdminPanel.tsx
@@ -7,6 +7,8 @@ import { useNavigate } from 'react-router-dom';
 import { Doctor } from '../../types/Doctor';
 import { deleteDoctor, getAllDoctors } from '../../services/doctorsService';
 import { DoctorItem } from './DoctorItem/DoctorItem';
+import { debounce } from 'lodash';
+import { FILTER_DELAY } from './constants';
 
 interface AdminPanelProps {
   admin: User;
@@ -16,10 +18,54 @@ export const AdminPanel = React.memo(({ admin }: AdminPanelProps) => {
   const navigate = useNavigate();
   const [users, setUsers] = React.useState<User[]>([]);
   const [doctors, setDoctors] = React.useState<Doctor[]>([]);
-
+  const [userFilter, setUserFilter] = React.useState<string>('');
+  const [doctorFilter, setDoctorFilter] = React.useState<string>('');
+  const [filteredDoctors, setFilteredDoctors] = React.useState<Doctor[]>([]);
+  const [filteredUsers, setFilteredUsers] = React.useState<User[]>([]);
   const [selectedUser, setSelectedUser] = React.useState<User | null>(null);
+
   const [selectedDoctor, setSelectedDoctor] = React.useState<Doctor | null>(
     null,
+  );
+
+  const setFilteredDoctorsDebounced = React.useRef(
+    debounce((filter: string, doctors: Doctor[]) => {
+      const loweredFilter = filter.toLowerCase();
+      setFilteredDoctors(
+        doctors.filter(
+          (doctor) =>
+            doctor.name.toLowerCase().includes(loweredFilter) ||
+            doctor.login.toLowerCase().includes(loweredFilter),
+        ),
+      );
+    }, FILTER_DELAY),
+  );
+
+  const setFilteredUsersDebounced = React.useRef(
+    debounce((filter: string, users: User[]) => {
+      const loweredFilter = filter.toLowerCase();
+      setFilteredUsers(
+        users.filter(
+          (user) =>
+            user.name.toLowerCase().includes(loweredFilter) ||
+            user.login.toLowerCase().includes(loweredFilter),
+        ),
+      );
+    }, FILTER_DELAY),
+  );
+
+  const onDoctorFilterChange = React.useCallback(
+    (event: React.FormEvent<HTMLInputElement>) => {
+      setDoctorFilter(event.currentTarget.value);
+    },
+    [],
+  );
+
+  const onUserFilterChange = React.useCallback(
+    (event: React.FormEvent<HTMLInputElement>) => {
+      setUserFilter(event.currentTarget.value);
+    },
+    [],
   );
 
   React.useEffect(() => {
@@ -27,6 +73,7 @@ export const AdminPanel = React.memo(({ admin }: AdminPanelProps) => {
       const response = await getAllUsers();
       if (response.ok) {
         setUsers(response.data);
+        setFilteredUsers(response.data);
       }
     };
 
@@ -34,12 +81,21 @@ export const AdminPanel = React.memo(({ admin }: AdminPanelProps) => {
       const response = await getAllDoctors();
       if (response.ok) {
         setDoctors(response.data);
+        setFilteredDoctors(response.data);
       }
     };
 
     fetchDoctors();
     fetchUsers();
   }, []);
+
+  React.useEffect(() => {
+    setFilteredDoctorsDebounced.current(doctorFilter, doctors);
+  }, [doctorFilter, doctors]);
+
+  React.useEffect(() => {
+    setFilteredUsersDebounced.current(userFilter, users);
+  }, [userFilter, users]);
 
   const onUserSelect = React.useCallback((user: User) => {
     setSelectedUser(user);
@@ -82,8 +138,13 @@ export const AdminPanel = React.memo(({ admin }: AdminPanelProps) => {
     <div className="panel-container">
       <h3>Администратор {admin.name}</h3>
       <h4>Врачи: </h4>
+      <input
+        placeholder="Найти по имени, логину"
+        value={doctorFilter}
+        onChange={onDoctorFilterChange}
+      />
       <div className="users-container">
-        {doctors.map((doctor) => (
+        {filteredDoctors.map((doctor) => (
           <DoctorItem
             doctor={doctor}
             opened={selectedDoctor?.id === doctor.id}
@@ -97,8 +158,13 @@ export const AdminPanel = React.memo(({ admin }: AdminPanelProps) => {
         Добавить врача
       </button>
       <h4>Пользователи: </h4>
+      <input
+        placeholder="Найти по имени, логину"
+        value={userFilter}
+        onChange={onUserFilterChange}
+      />
       <div className="users-container">
-        {users.map((user) => (
+        {filteredUsers.map((user) => (
           <UserItem
             user={user}
             opened={selectedUser?.id === user.id}
